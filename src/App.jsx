5b0b3a51c755f662582a7de8b9b35960ad8f96e6
leebut <lee@leesenglish.com>
2023-12-08 12:01:37 +0800

    Update: aesthetic changes

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ export default function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-purple-600">Currency Converter</h1>
+      <h1 className="text-5xl font-bold text-purple-600">Currency Converter</h1>
 
       <ConvertBox>
         <Amount
